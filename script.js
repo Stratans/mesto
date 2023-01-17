0f1db86fbf3e1c464e474216fa14fbf3661f0f06
Dmitry Stratan <streitan@yandex.ru>
2023-01-17 17:34:03 +0300

    fix: багфикс попапа

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let inputAbout = document.querySelector('.popup__input_type_about');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-// Открываем попап
+
 let popupOpen = function() {
 	popupEl.classList.add('popup_opened');
 	inputName.value = profileName.textContent;
 	inputAbout.value = profileJob.textContent;
 }
 
-// Закрываем попап
+
 let popupClose = function() {
 	popupEl.classList.remove('popup_opened');
 }
