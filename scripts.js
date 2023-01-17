f28f7a24d5a17faa5fb59630334749e122deeba7
Dmitry Stratan <streitan@yandex.ru>
2023-01-17 14:07:30 +0300

    feat: оформить файл ридми

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//Объявляем переменные
+let popupEl = document.querySelector('popup');
+let popupCloseBtnEl = document.querySelector('popup__close');
+let popOpenBtnEl = document.querySelector('');
+
+console.log ('popupEl');
