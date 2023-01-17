314acb0e0166e284c6f91b48b5c845126a7e66d6
Dmitry Stratan <streitan@yandex.ru>
2023-01-17 15:49:04 +0300

    fix: добавить адаптив для попапа

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
-//Объявляем переменные
-let popupEl = document.querySelector('popup');
-let popupCloseBtnEl = document.querySelector('popup__close');
-let popOpenBtnEl = document.querySelector('');
+// //Объявляем переменные
+// let popupEl = document.querySelector('.popup');
+// let popupCloseBtnEl = document.querySelector('.popup__close');
+// let popupOpenBtnEl = document.querySelector('.profile__edit-btn');
+// let formEl = document.querySelector('.popup__form');
+// let inputName = document.querySelector('.popup__input_type_name');
+// let inputAbout = document.querySelector('.popup__input_type_about');
+// let profileInfoTitle = document.querySelector('.profile__info-title');
+// let profileInfoSubTitle = document.querySelector('.profile__info-subtitle');
 
-console.log ('popupEl');
+// // Открываем попап
+
+// let popupOpen = function() {
+	
+// }
+
+
+
+
+
+
+// // Закрываем попап
+
