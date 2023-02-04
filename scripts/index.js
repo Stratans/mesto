6f4b376253b39fdb39e8ca0fffef4eedcaa51fe3
Dmitry Stratan <streitan@yandex.ru>
2023-02-04 19:22:17 +0300

    feat: доделать скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,53 @@
-// Константы для темплейта и секции Elements
-const elementTemplate = document.querySelector('#element-template').content.querySelector('.element'); // Находим темплейт, присваиваем ему карточку
-const elementsBlock = document.querySelector('.elements'); // Находим секцию elements
-
-// Константы для формы редактирования
+// константы для темплейта и секции Elements
+const elementTemplate = document.querySelector('#element-template').content.querySelector('.element'); // находим темплейт, присваиваем ему карточку
+const elementsBlock = document.querySelector('.elements'); // находим секцию elements
+// константы для формы редактирования
 const popupEdit = document.querySelector('.popup_type_edit'); // Находим окно формы редактирования
 const popupOpenEditBtn = document.querySelector('.profile__edit-btn'); // Кнопка «редактировать»
-const formEdit = document.querySelector('.form_type_edit'); // находим форму редактирования профиля
+const formEdit = document.querySelector('.popup__form_type_edit'); // находим форму редактирования профиля
 const inputName = document.querySelector('.popup__input_profile_name'); // Инпут ФИО
 const inputAbout = document.querySelector('.popup__input_profile_about'); // Инпут Должность
 const profileName = document.querySelector('.profile__name'); // ФИО юзера
-const profileJob = document.querySelector('.profile__job'); // Должность юзера
-
-// Константы для добавления места
+const profileJob = document.querySelector('.profile__job'); // Должность юзер
+// константы для добавления места
 const popupAdd = document.querySelector('.popup_type_add');
-const formAdd = document.querySelector('.form_type_add');
+const formAdd = document.querySelector('.popup__form_type_add');
 const elementInputName = document.querySelector('.popup__input_place_name');
 const elementInputLink = document.querySelector('.popup__input_place_link');
 const elementBtnAdd = document.querySelector('.profile__add-btn');
-
-// Константа для закрытия попапа
+// константа для закрытия попапа
 const closeBtns = document.querySelectorAll('.popup__close');
+// константы для открытия попапа с картинкой
+const popupPreview = document.querySelector('.popup_type_show');
+const popupPreviewImg = document.querySelector('.popup__photo');
+const popupPreviewSubtitle = document.querySelector('.popup__subtitle');
 
-// Ищем родителя крестика и вызываем функцию закрытия попапа по родителю крестика
-closeBtns.forEach(btn => {
+
+closeBtns.forEach(btn => { // ищем родителя крестика и вызываем функцию закрытия попапа по родителю крестика
 	btn.addEventListener('click', (evt) => {
 		popupClose(evt.target.closest('.popup'));
 	});
 })
 
-// Функция создания карточки
+// функция создания карточки
 function createElement(item) {
 	const elementCard = elementTemplate.cloneNode(true); // клонируем темплейт
 	const elementCardName = elementCard.querySelector('.element__title'); // переменная заголовок карточки
-	const elementCardImg = elementCard.querySelector('.element__img'); // переменная картинку карточки
+	const elementCardImg = elementCard.querySelector('.element__img'); // переменная картинка карточки
 	const elementBtnTrash = elementCard.querySelector('.element__btn-trash'); // переменная иконка корзины
 	const elementBtnLike = elementCard.querySelector('.element__btn-like'); // переменная иконка лайка
 	elementCardName.textContent = item.name; // предоставляем доступ к тексту внутри элемента
 	elementCardImg.src = item.link; // берем атрибут src
 	elementCardImg.alt = item.name; // берем атрибут alt
-	elementBtnTrash.addEventListener('click', () => elementCard.remove()); // Слушатель для иконки корзины + Стрелочная функция "удаление карточки"
-	elementBtnLike.addEventListener('click', () => elementBtnLike.classList.toggle('element__btn-like_active')); // Слушатель для иконки лайка + Стрелочная функция лайк/дизлайк 
-	// картинка 
+	elementBtnTrash.addEventListener('click', () => elementCard.remove()); // слушатель для иконки корзины + стрелочная функция "удаление карточки"
+	elementBtnLike.addEventListener('click', () => elementBtnLike.classList.toggle('element__btn-like_active')); // слушатель для иконки лайка + стрелочная функция лайк/дизлайк 
+
+	elementCardImg.addEventListener('click', () => {
+		popupPreviewImg.src = item.link;
+		popupPreviewImg.alt = item.name;
+		popupPreviewSubtitle.textContent = item.name;
+		popupOpen(popupPreview);
+	});
 	return elementCard;
 };
 
@@ -54,7 +61,7 @@ const popupClose = function(popupName) {
 	popupName.classList.remove('popup_opened');
 };
 
-// Рендер карточки
+// Функция для рендера карточки
 function renderElement(card) {
 	elementsBlock.prepend(createElement(card)); // Добаваляем методом prepend карточку
 };
@@ -62,19 +69,16 @@ function renderElement(card) {
 // Рендер начального массива
 	initialCards.forEach(item => renderElement(item)); // Перебираем методом forEach начальный массив
 
-popupOpenEditBtn.addEventListener('click', () => {
+// Слушатели:
+popupOpenEditBtn.addEventListener('click', () => { // Слушатель для кнопки открытия попапа
 	popupOpen(popupEdit);
 	inputName.value = profileName.textContent;
 	inputAbout.value = profileJob.textContent;
-}); // Слушатель для кнопки открытия попапа
-
-
+}); 
 formEdit.addEventListener('submit', handleFormSubmitEdit); // Слушатель для кнопки «сохранить»
-
 elementBtnAdd.addEventListener('click', () => { // Слушатель на иконку плюса
 	popupOpen(popupAdd);
 });
-
 formAdd.addEventListener('submit', handleFormSubmitAdd); // Слушатель сабмита на форму добавления
 
 // Функция для отправки формы
